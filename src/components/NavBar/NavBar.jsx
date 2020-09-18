06532710a81a542758640b78999f9c9df1fab09f
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-09-18 10:08:43 -0400

    adding schemas and nav css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = ({ user, handleLogout }) => {
     return (
     <>
+        <nav>
+          <div className="nav-wrapper">
       {user ?
-        <nav>
-          <div className="nav-wrapper">
-            <ul id="nav-mobile" className="right">
-              <li><a href=" " className="nav-link">Welcome, {user.name}</a></li>
-              <li><a href="/users" className="nav-link">Users</a></li>
+            <>
+              <a href="/" className="nav-link">Welcome, {user.name}</a>
+              <a href="/users" className="nav-link">Users</a>
               <Link to='' className='NavBar-link' onClick={handleLogout}>LOG OUT</Link>
-            </ul>
-          </div>
-        </nav>
+            </>
       :
-        <nav>
-          <div className="nav-wrapper">
-            <ul id="nav-mobile" className="right">
-              <li><a href="/login" className="nav-link">Log In</a></li>
-              <li><a href="/signup" className="nav-link">Sign Up</a></li>
-            </ul>
+            <>
+              <a href="/login" className="nav-link">Log In</a>
+              <a href="/signup" className="nav-link">Sign Up</a>
+            </>
+      }
           </div>
         </nav>
-      }
     </>
   )
 }
